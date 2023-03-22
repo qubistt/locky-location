83af9bf13efcd074a71003ce67dac9a7486e6f24
Qubisted <78350166+Qubisted@users.noreply.github.com>
2023-03-22 15:07:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const ipAddressElement = document.getElementById('ip-address');
+
+async function getIpAddress() {
+  const response = await fetch('https://api.ipify.org?format=json');
+  const data = await response.json();
+  return data.ip;
+}
+
+async function displayIpAddress() {
+  const ipAddress = await getIpAddress();
+  ipAddressElement.textContent = `Your public IP address is: ${ipAddress}`;
+}
+
+displayIpAddress();
